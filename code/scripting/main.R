9258c1c85e0d401cb39de6ba1059d341c943cd2f
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2023-10-10 16:52:28 +0100

    more exploration

--- code/scripting/main.R
+++ code/scripting/main.R
@@ -9,25 +9,16 @@ data <- OpenML::getOMLDataSet(data.id = 41214)
 
 data$desc$description
 
-cars <- data$dat
+cars <- data$data
 
 cars_split <- initial_split(cars, prop = 0.8)
 train <- training(cars_split)
 test <- testing(cars_split)
 
-glm1 <- glm(ClaimNb ~ Exposure + Area + VehPower + VehAge + DrivAge + BonusMalus + VehBrand + VehGas + Density + Region, data = test)
+glm1 <- glm(ClaimNb ~ Exposure + Area + VehPower + VehAge + DrivAge + BonusMalus + VehBrand + VehGas + Density + Region, data = train, family = quasipoisson(link=log))
 summary(glm1)
 anova(glm1)
 
-predictions <- predict(glm1, test) %>% 
-  data.frame() %>% 
-  rowid_to_column() %>% 
-  mutate("predictions" = .[]) %>% 
-  select(rowid, predictions) %>% 
-  right_join(test, join_by(rowid == IDpol))
+write_csv(cars, "data/output/cars.csv")
 
-rmse(predictions, ClaimNb, predictions)
-
-
-View(predictions)
-?rename
+predictions = predict(glm1, test, type = "response")
