09a49460855f650ef47ea2cdf1a055633a7c0977
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2023-10-11 16:10:38 +0100

    more exploration

--- code/scripting/tidymodels_testing.R
+++ code/scripting/tidymodels_testing.R
@@ -8,3 +8,16 @@ library(tidymodels)
 data <- OpenML::getOMLDataSet(data.id = 41214)
 
 data$desc$description
+cars <- data$desc$description
+
+cars_split <- initial_split(cars, prop = 0.8)
+train <- training(cars_split)
+test <- testing(cars_split)
+
+model_recipe <- train %>% 
+  recipe(ClaimNb ~.) %>% 
+  update_role(IDpol, new_role = "id_variable") %>% 
+  step_dummy(all_factor())
+
+
+bake(model_recipe %>% prep(), train)
