0987323bc6453d6e719f22afd95b1dd650bcc993
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2023-10-16 15:22:41 +0100

    testing plots

--- code/scripting/glm_testing.R
+++ code/scripting/glm_testing.R
@@ -3,6 +3,7 @@ library(OpenML)
 library(farff)
 library(tidyverse)
 library(tidymodels)
+library(ggplot2)
 
 
 data <- OpenML::getOMLDataSet(data.id = 41214)
@@ -25,4 +26,23 @@ write_csv(cars, "data/output/cars.csv")
 
 predictions = predict(glm1, test, type = "response")
 
+
+reg_count <- ggplot(cars, aes(x = Region)) +
+  geom_bar() +
+  labs(title = "Count of Cars by Region", x = "Region", y = "Count")
+
+area_count <- ggplot(cars, aes(x = Area)) +
+  geom_bar() +
+  labs(title = "Count of Cars by Region", x = "Area", y = "Count")
+
+brand_count <- ggplot(cars, aes(x = VehBrand)) +
+  geom_bar() +
+  labs(title = "Count of Cars by Region", x = "VehBrand", y = "Count")
+
+gas_count <- ggplot(cars, aes(x = Region)) +
+  geom_bar() +
+  labs(title = "Count of Cars by Region", x = "VehGas", y = "Count")
+
+grid_plot <- reg_count + area_count + brand_count + gas_count + 
+  plot_layout(nrow = 2, byrow = TRUE)
 
