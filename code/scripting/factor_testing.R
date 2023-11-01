318aec73fa28ceb867679e88f5f4de841f2206a2
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2023-11-01 13:13:27 +0000

    tesing factor types

--- code/scripting/factor_testing.R
+++ code/scripting/factor_testing.R
@@ -8,13 +8,16 @@ as.numeric(levels(cars$Region))
 as.numeric(levels(cars$Region))
 
 unclass(cars$Region) %>%  view()
-factor(cars$Region, levels = c(1length(cars$IDpol)
+
 
 boxplot(cars$Exposure)
 abline(h=1)
-unique(cars$Region)
+
 
 cbind(cars$Region %>%  unclass,
       cars_geo$region_name)
 
 factor(cars_geo$region_name, levels = sort(unique(cars_geo$region_name))) %>% unclass()
+
+
+
