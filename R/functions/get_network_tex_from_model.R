242190803aa698b50fbc939fc55f3347e80daf6f
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-02-12 19:24:53 +0000

    cosmetic changes

--- R/functions/get_network_tex_from_model.R
+++ R/functions/get_network_tex_from_model.R
@@ -2,13 +2,12 @@ get_network_tex_from_model <- function(model,
                                        margin = 0, 
                                        h_scale = 1, 
                                        v_scale = 1) {
-  get_network_tex(
-    n_input = best_model$model$layers[[1]]$input_shape[[2]],
-    n_hidden1 = best_model$model$layers[[2]]$input_shape[[2]],
-    n_hidden2 = best_model$model$layers[[3]]$input_shape[[2]],
+  c(get_network_tex(
+    n_input = model$model$layers[[1]]$input_shape[[2]],
+    n_hidden1 = model$model$layers[[2]]$input_shape[[2]],
+    n_hidden2 = model$model$layers[[3]]$input_shape[[2]],
     margin = margin,
     h_scale = h_scale,
-    v_scale = v_scale,
-    name = name
-  )
+    v_scale = v_scale
+  ))
 }
