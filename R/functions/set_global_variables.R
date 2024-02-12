6b84cacd8bc47a92760862f08d239a291c86e865
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-02-12 19:24:09 +0000

    add function for settings

---
+++ R/functions/set_global_variables.R
@@ -0,0 +1,9 @@
+set_global_variables <- function(verbose = 1,
+                                 source = FALSE) {
+  # By default, runs all code chunks and prints all outputs
+  # Undesirable for report knitting,
+  # Desirable for testing and validation
+
+  global_variables <<- list(verbose = verbose,
+                            source = source)
+}
