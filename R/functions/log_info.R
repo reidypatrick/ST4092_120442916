a4a10be12f0c81a4ca8712089b8877cf3eb81e8c
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-02-14 21:31:31 +0000

    update some functions to reflect global variables

--- R/functions/log_info.R
+++ R/functions/log_info.R
@@ -1,5 +1,5 @@
-log_info <- function(message) {
-  if (global_variables$verbose == 1) {
+log_info <- function(message, verbose = get_global_variables(source)) {
+  if (verbose == 1) {
     cat(
       paste("[INFO][",
         substr(Sys.time(), 12, 19),
