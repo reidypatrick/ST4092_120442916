47ce95698ba39d642da02c097a353445b5ec1cca
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-02-19 22:29:55 +0000

    fix create_map and add logging

--- R/functions/create_map.R
+++ R/functions/create_map.R
@@ -1,4 +1,3 @@
-# nolint start
 create_map <- function(.data = cars_orig, .col, .shape = shape) {
   # Defaults to using cars_orig and france shapefile
   # also defaults to summarising by the sum of specified column
@@ -8,24 +7,29 @@ create_map <- function(.data = cars_orig, .col, .shape = shape) {
   # TODO: create separate function for summarising:
   #      args: .data, group_by, fun(s), ...?
   # summarise desired column from cars data set
+  log_info("Summarise data")
   cars_data <- .data %>%
     group_by(Region) %>%
     summarise(col = sum({{ .col }})) %>%
     mutate(Region = substr(Region, 2, 3))
 
+  log_info("Join to shapefile")
   # join onto .shape data
   map_obj <- shape_copy@data %>%
     left_join(cars_data, join_by(CCA_1 == Region)) %>%
     dplyr::select(ID_1, col) %>%
     inner_join(sf::st_as_sf(shape_copy), join_by(ID_1))
 
+  log_info("Plot Map")
   # plot according to desired column
   ggplot() +
     geom_sf(map_obj,
       mapping = aes(fill = col, geometry = geometry),
       colour = "white"
     ) +
-    scale_fill_gradient(name = substitute(.col), labels = "comma") +
+    scale_fill_gradient(name = substitute(.col)) +
     theme_void()
+  
+  log_info("Done create_map")
 }
-# nolint end
+
