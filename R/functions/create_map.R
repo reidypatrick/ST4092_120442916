72d7f5d8c07e0811e5accff2eb9e5a6be4a97cec
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-02-19 22:36:47 +0000

    update comments and add global option

--- R/functions/create_map.R
+++ R/functions/create_map.R
@@ -1,35 +1,33 @@
 create_map <- function(.data = cars_orig, .col, .shape = shape) {
   # Defaults to using cars_orig and france shapefile
-  # also defaults to summarising by the sum of specified column
+  # also defaults to sum
 
   shape_copy <- .shape
 
-  # TODO: create separate function for summarising:
-  #      args: .data, group_by, fun(s), ...?
-  # summarise desired column from cars data set
-  log_info("Summarise data")
-  cars_data <- .data %>%
-    group_by(Region) %>%
-    summarise(col = sum({{ .col }})) %>%
-    mutate(Region = substr(Region, 2, 3))
+  if (getOption("source_or_run") == "run") {
+    log_info("Summarise data")
+    cars_data <- .data %>%
+      group_by(Region) %>%
+      summarise(col = sum({{ .col }})) %>%
+      mutate(Region = substr(Region, 2, 3))
 
-  log_info("Join to shapefile")
-  # join onto .shape data
-  map_obj <- shape_copy@data %>%
-    left_join(cars_data, join_by(CCA_1 == Region)) %>%
-    dplyr::select(ID_1, col) %>%
-    inner_join(sf::st_as_sf(shape_copy), join_by(ID_1))
+    log_info("Join to shapefile")
+    # join onto .shape data
+    map_obj <- shape_copy@data %>%
+      left_join(cars_data, join_by(CCA_1 == Region)) %>%
+      dplyr::select(ID_1, col) %>%
+      inner_join(sf::st_as_sf(shape_copy), join_by(ID_1))
 
-  log_info("Plot Map")
-  # plot according to desired column
-  ggplot() +
-    geom_sf(map_obj,
-      mapping = aes(fill = col, geometry = geometry),
-      colour = "white"
-    ) +
-    scale_fill_gradient(name = substitute(.col)) +
-    theme_void()
-  
-  log_info("Done create_map")
+    log_info("Plot Map")
+    # plot according to desired column
+    ggplot() +
+      geom_sf(map_obj,
+        mapping = aes(fill = col, geometry = geometry),
+        colour = "white"
+      ) +
+      scale_fill_gradient(name = substitute(.col)) +
+      theme_void()
+
+    log_info("Done create_map")
+  }
 }
-
