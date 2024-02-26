e1dad5cbdf988a078cd83798e7f68e955ccf36df
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-02-26 15:24:52 +0000

    remove options control and final log statement

--- R/functions/create_map.R
+++ R/functions/create_map.R
@@ -1,33 +1,33 @@
-create_map <- function(.data, .col, .shape) {
+create_map <- function(
+    .data,
+    .col,
+    .shape,
+    source_or_run = getOption("source_or_run")) {
   # Defaults to using cars_orig and france shapefile
   # also defaults to sum
 
   shape_copy <- .shape
 
-  if (getOption("source_or_run") == "run") {
-    log_info("Summarise data")
-    cars_data <- .data %>%
-      group_by(Region) %>%
-      summarise(col = sum({{ .col }})) %>%
-      mutate(Region = substr(Region, 2, 3))
+  log_info("Summarise data")
+  cars_data <- .data %>%
+    group_by(Region) %>%
+    summarise(col = mean({{ .col }})) %>%
+    mutate(Region = substr(Region, 2, 3))
 
-    log_info("Join to shapefile")
-    # join onto .shape data
-    map_obj <- shape_copy@data %>%
-      left_join(cars_data, join_by(CCA_1 == Region)) %>%
-      dplyr::select(ID_1, col) %>%
-      inner_join(sf::st_as_sf(shape_copy), join_by(ID_1))
+  log_info("Join to shapefile")
+  # join onto .shape data
+  map_obj <- shape_copy@data %>%
+    left_join(cars_data, join_by(CCA_1 == Region)) %>%
+    dplyr::select(ID_1, col) %>%
+    inner_join(sf::st_as_sf(shape_copy), join_by(ID_1))
 
-    log_info("Plot Map")
-    # plot according to desired column
-    ggplot() +
-      geom_sf(map_obj,
-        mapping = aes(fill = col, geometry = geometry),
-        colour = "white"
-      ) +
-      scale_fill_gradient(name = substitute(.col)) +
-      theme_void()
-
-    log_info("Done create_map")
-  }
+  log_info("Plot Map")
+  # plot according to desired column
+  ggplot() +
+    geom_sf(map_obj,
+      mapping = aes(fill = col, geometry = geometry),
+      colour = "white"
+    ) +
+    scale_fill_gradient(name = substitute(.col)) +
+    theme_void()
 }
